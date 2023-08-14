c4415b0f653b86671df1036cfc8985425cba41b2
Essi <59375095+essivarjonen@users.noreply.github.com>
2023-08-14 08:43:24 +0300

    added quiz game

---
+++ quiz_game.py
@@ -0,0 +1,11 @@
+print("Welcome to Quiz Game!")
+
+playing = input("Do you want to play? (yes/no) ")
+
+if playing != "yes":
+    quit("ok, byee!")
+
+print("Let's play the Quiz Game!")
+
+userName = input("First enter your username ")
+print("Hello " + userName + "!")
